bf359fc0328f4a4616f23a8e30f542f134497c9e
hwijoon <chjcoder@gmail.com>
2025-07-07 17:48:27 +0900

    init

---
+++ src/main/java/com/example/beesillee/BeesilleeApplication.java
@@ -0,0 +1,13 @@
+package com.example.beesillee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeesilleeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeesilleeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beesillee/BeesilleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beesillee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeesilleeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
